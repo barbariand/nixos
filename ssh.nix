7b587b2e0dc918db3fa032783a3b01c14f010eec
Cindy Nilsson <cindy@simd.me>
2025-09-02 09:55:55 +0200

    adding host key for home

--- ssh.nix
+++ ssh.nix
@@ -1,7 +1,7 @@
 let
   github = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIdaQe2WbF2N7kEzrcz2JNb7C+LdwVdnIH2L8Ok5Zfqh";
   lenovo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsK1mn4aCbtTviKH9EgYBdQkue4NkiLyP2eiMKbqr31";
-  home_computer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAtTqXhEwlDPmpAQuShQlprHQn6iD4ODnzFEaRTtwUC";
+  home_computer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATgAc9Jsy22N5pB/+q71N9glTV4rlKnWeMd+RE/qFSf";
 in {
   users = [lenovo home_computer github];
   systems = [];
