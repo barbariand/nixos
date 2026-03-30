b6308f0b3e919426cc614b22a8607738a132b5e6
barbariand <cindy@simd.me>
2026-03-30 21:32:49 +0200

    updating packages

--- hosts/raspberrypi/hardware-configuration.nix
+++ hosts/raspberrypi/hardware-configuration.nix
@@ -15,6 +15,10 @@
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
+  # todo: remove this when this is fixed: https://github.com/NixOS/nixpkgs/issues/154163
+  # related: https://github.com/NixOS/nixpkgs/issues/109280
+  # related: https://discourse.nixos.org/t/cannot-build-raspberry-pi-sdimage-module-dw-hdmi-not-found/71804
+  boot.initrd.allowMissingModules = true;
   boot.initrd.availableKernelModules = ["xhci_pci"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
